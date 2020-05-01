4a135ce647d0dcc77a4a57aa0ed709071e8d0c74
Vargas1723 <vargas23.bv@gmail.com>
2020-05-01 18:32:26 -0500

    Reset Password
    Send email to reset password

--- Animus/app/src/main/java/com/happynacho/animus/gLogin.java
+++ Animus/app/src/main/java/com/happynacho/animus/gLogin.java
@@ -1,5 +1,6 @@
 package com.happynacho.animus;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -11,9 +12,12 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
@@ -21,7 +25,7 @@ import com.google.firebase.auth.FirebaseAuth;
 public class gLogin extends AppCompatActivity {
     private EditText mEmail, mPassword;
     private Button mLoginBtn;
-    private TextView mCreateBtn;
+    private TextView mCreateBtn, mForget;
     private FirebaseAuth fAuth;
     private ProgressBar progressBar;
 
@@ -43,6 +47,7 @@ public class gLogin extends AppCompatActivity {
         this.fAuth          = FirebaseAuth.getInstance();
         this.mLoginBtn      = findViewById(R.id.loginBtn);
         this.mCreateBtn     = findViewById(R.id.textViewCreate);
+        this.mForget        = findViewById(R.id.textViewForget);
 
 
         mLoginBtn.setOnClickListener(new View.OnClickListener() {
@@ -85,6 +90,42 @@ public class gLogin extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(getApplicationContext(),Register.class));
+            }
+        });
+        mForget.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final EditText resetMail = new EditText(v.getContext());
+                final AlertDialog.Builder passwordResetDialog = new AlertDialog.Builder(v.getContext());
+                passwordResetDialog.setTitle("Reset Password?");
+                passwordResetDialog.setMessage("Enter your email to received Reset Link.");
+                passwordResetDialog.setView(resetMail);
+
+                passwordResetDialog.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // extract the email and send reset link
+                        String mail = resetMail.getText().toString();
+                        fAuth.sendPasswordResetEmail(mail).addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+                                Toast.makeText(gLogin.this,"Reset Link Sent to Your Email",Toast.LENGTH_SHORT).show();
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(gLogin.this,"Error ! Reset Link is Not Sent "+ e.getMessage(),Toast.LENGTH_SHORT);
+                            }
+                        });
+                    }
+                });
+                passwordResetDialog.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // close the dialog
+                    }
+                });
+                passwordResetDialog.create().show();
             }
         });
 
